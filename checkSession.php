7d5f03d0e95dbc09f015da919a52bdcf0c0e3e2e
Zen Nakamura <zenzenzen@gmail.com>
2021-04-20 06:01:55 -0400

    modded checkSession.php

--- checkSession.php
+++ checkSession.php
@@ -9,28 +9,30 @@ if(!isset($_SESSION)){
 }; 
 
 // AJAX call to set name from HomePage
-$sessName = $_POST['sessName'];
-if ($sessName == 'goDo'){
-	?><script type="text/javascript">alert( "sessName received from footerJS");
-	alert("logged name: " <?php echo  $_SESSION['logged_user']; ?> );
-	</script>
-	<?php
-	// $_SESSION['logged_user'] = htmlentities($_POST['user_name']); // user-icon name set here
-	if (isset($_SESSION['logged_user'])){
-		$userName = $_SESSION['logged_user'];
-		// echo "Session data exists<br>";
+if (isset($_POST['sessName'])){
+	// $sessName = $_POST['sessName'];
+	// if ($sessName == 'goDo'){
+		?><script type="text/javascript">alert( "sessName received from footerJS");
+		alert("logged name: " <?php echo  $_SESSION['logged_user']; ?> );
+		</script>
+		<?php
+		// $_SESSION['logged_user'] = htmlentities($_POST['user_name']); // user-icon name set here
+		if (isset($_SESSION['logged_user'])){
+			$userName = $_SESSION['logged_user'];
+			// echo "Session data exists<br>";
 
-		echo $userName; // Where doees this go? Where do you see this?
+			echo $userName; // Where doees this go? Where do you see this?
 
-	}
-	// print_r($_SESSION); // when homePage loads, this is to check array
-	// echo "sessName";
+		}
+		// print_r($_SESSION); // when homePage loads, this is to check array
+		// echo "sessName";
 
-	else {
-		echo "didn't receive sessName from footerJS";
-	$noSession = "No session data<br>";
-	echo $noSession;
-	print_r($_SESSION);
-	}
+		else {
+			echo "didn't receive sessName from footerJS";
+		$noSession = "No session data<br>";
+		echo $noSession;
+		print_r($_SESSION);
+		}
+	// }
 }
 ?>
