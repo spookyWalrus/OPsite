db61406c65f50a58fc012b93a24681b742141229
Zen Nakamura <zenzenzen@gmail.com>
2021-04-20 06:50:43 -0400

    trying to get footerJS to work commented out insertRow script

--- footer.php
+++ footer.php
@@ -18,7 +18,7 @@ if (isset($_POST['killSess'])){
             <!-- ======================  my own JS here ========= -->
          <!--  <script type="text/javascript" src="jQuery.js"></script> -->
 <!-- <script type="text/javascript" src="js/footerJS.js"></script> -->
- <script type="text/javascript" src="js/OPsite-insertRow.js"></script>
+ <!-- <script type="text/javascript" src="js/OPsite-insertRow.js"></script> -->
 <script type="text/javascript"  src="js/footerJS.js">
 alert('calling sessionName()');
 // sessionName();
