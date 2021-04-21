357817fcbe695781daafda594539c3f3bcbea77c
Zen Nakamura <zenzenzen@gmail.com>
2021-04-21 19:43:49 -0400

    echo values

--- index.php
+++ index.php
@@ -23,9 +23,9 @@
                         <input class="btn btn-primary submitText" type="submit" name="login_submit" value="Submit your soul">
                         </div>
                     </form>
-                    <!-- <div id="loginErrorMsg"><?php echo $errors[0]; ?>-->
+                     <div id="loginErrorMsg"><?php echo $errors[0]; ?>
                     <!-- </div> -->
-                    <div id="loginErrorMsg"></div>
+                    <!-- <div id="loginErrorMsg"></div> -->
                 </div>
             </div>
         </div>
