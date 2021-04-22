7a904d1037194986c56c45e8eadd882bb1b2743f
Zen Nakamura <zenzenzen@gmail.com>
2021-04-22 15:42:09 -0400

    changed data sent via POST

--- js/footerJS.js
+++ js/footerJS.js
@@ -13,7 +13,7 @@ function login(){
         $.ajax({
             type: 'post', 
             url: 'indexTools.php',
-            data:'logAttempt', 
+            data: {logAttempt: 'dude}, 
             dataType: 'text',
             success: function (data) {  
               var loginYN = data;
