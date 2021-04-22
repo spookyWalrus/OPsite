4aeaf74fd45208f3e1c5f919bf05380e0bc32f38
Zen Nakamura <zenzenzen@gmail.com>
2021-04-22 14:31:32 -0400

    changed button to non-submit

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
             <div class="col-md-4 col-md-offset-4">
                 <div class="well" id="loginBox">
                     <!-- <form method="POST" action=""> -->
-                    <form method="" action="">
+                    <form method="POST" action="">
                         <div class="enterBox">User: &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;<input type="text" name="user_name" placeholder="uh... who?" >
                         </div>
                         <br>
@@ -22,7 +22,8 @@
                         </div>
                         <br>
                         <div >
-                        <input class="btn btn-primary submitText" type="submit" name="login_submit" value="Submit your soul" onclick="login()">
+                           <!--  <input class="btn btn-primary submitText" type="submit" name="login_submit" value="Submit your soul" > -->
+                        <input class="btn btn-primary submitText"  name="login_submit" value="Submit your soul" onclick="login()">
                         </div>
                     </form>
                     <div id="loginErrorMsg">
@@ -48,7 +49,7 @@
         <!-- // showError(); -->
     <!-- </script> -->
 
-   <script type="text/javascript" src="js/footerJS.js"></script>
+    <script type="text/javascript" src="js/footerJS.js"></script>
 
     </body>
 </html> 
