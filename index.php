22728c58987e47f4a7ab329b9c4137dfd595a389
Zen Nakamura <zenzenzen@gmail.com>
2021-04-22 08:51:04 -0400

    trying to get js to echo back value 46

--- index.php
+++ index.php
@@ -32,7 +32,8 @@
     </div>
 
    <script type="text/javascript">
-        var em = ' <?php echo json_encode($errorMess, JSON_HEX_TAG); ?> ';
+        // var em = ' <?php echo json_encode($errorMess, JSON_HEX_TAG); ?> ';
+        var em = "<?php echo json_encode($errorMess); ?>";
             var idem = document.querySelector("#loginErrorMsg");
         function showError(){
             if (em !== ''){
