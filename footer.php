ba0d8ff8d2f92165e9fa8c037bad889d347087d3
Zen Nakamura <zenzenzen@gmail.com>
2021-04-23 06:10:54 -0400

    trying to get Trex game to work

--- footer.php
+++ footer.php
@@ -1,20 +1,5 @@
-<?php include("checkSession.php"); 
+<?php include("checkSession.php"); ?>
 
-  // <!-- === log out behavoiur ============== -->
-
-// AJAX call to kill session
-if (isset($_POST['killSess'])){
-
-    unset($_SESSION['logged_user']);
-
-    // session_destroy();
-    $_SESSION = array(); 
-    if (!isset($_SESSION['logged_user'])){
-      // echo "session unset";
-    }
-    exit();
-}
-?>
             <!-- ======================  my own JS here ========= -->
          <!--  <script type="text/javascript" src="jQuery.js"></script> -->
 <script type="text/javascript" src="js/footerJS.js"></script>
