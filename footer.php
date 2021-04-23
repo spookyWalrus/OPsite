0a736a465580307eb615895a18a22606f3879bc2
Zen Nakamura <zenzenzen@gmail.com>
2021-04-23 06:34:22 -0400

    deleted some empty lines in footer

--- footer.php
+++ footer.php
@@ -1,15 +1,11 @@
 <?php include("checkSession.php"); ?>
 
-            <!-- ======================  my own JS here ========= -->
-         <!--  <script type="text/javascript" src="jQuery.js"></script> -->
+<!-- ======================  my own JS here ========= -->
 <script type="text/javascript" src="js/footerJS.js"></script>
  <script type="text/javascript" src="js/OPsite-insertRow.js"></script>
-<!-- <script type="text/javascript"  src="js/footerJS.js"> -->
 <script type="text/javascript">
-// alert('calling sessionName()');
 sessionName();
 </script>
-
 
 	</body>
 </html> 
